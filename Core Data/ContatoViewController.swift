fbc5b17877ad3862df7ba08a59669387c3c31f88
Usuário Convidado <Guest@L2602MICRO09.local>
2020-09-17 22:47:23 -0300

    Initial commit

--- Core Data/ContatoViewController.swift
+++ Core Data/ContatoViewController.swift
@@ -7,9 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
-class ContatoViewController: UIViewController {
+class ContatoViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtNome: UITextField!
+    @IBOutlet weak var txtTelefone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == txtNome{
+            txtTelefone.becomeFirstResponder()
+            return true
+        } else if textField == txtTelefone{
+            txtEmail.becomeFirstResponder()
+            return true
+        } else if textField == txtEmail {
+            Salvar(textField)
+            return true
+        }
+        
+        return false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +37,32 @@ class ContatoViewController: UIViewController {
     }
     
 
+    @IBAction func Salvar(_ sender: Any) {
+        txtNome.resignFirstResponder()
+        txtTelefone.resignFirstResponder()
+        txtEmail.resignFirstResponder()
+        
+        if txtNome.text!.isEmpty || txtTelefone.text!.isEmpty || txtEmail.text!.isEmpty{
+            let alerta = UIAlertController(title: "Erro", message: "Preencha todos os campos", preferredStyle: UIAlertController.Style.alert)
+            
+            alerta.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            
+            present(alerta, animated: true, completion: nil)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        txtNome.resignFirstResponder()
+        txtTelefone.resignFirstResponder()
+        txtEmail.resignFirstResponder()
+    }
+    
+    
+    func save(nome: String, tele: String, emai: String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        
+    }
+    
     /*
     // MARK: - Navigation
 
